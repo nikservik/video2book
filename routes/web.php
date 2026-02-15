7c697724615571656558487dad5114f56f7af1ac
Sergey Nikiforov <ser.nikiforov@gmail.com>
2026-02-15 04:29:17 +0200

    Удаление лишних файлов

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Livewire\HomePage;
 use App\Livewire\PipelineShowPage;
 use App\Livewire\PipelinesPage;
-use App\Livewire\PipelineStepPage;
-use App\Livewire\ProjectLessonPage;
 use App\Livewire\ProjectRunPage;
 use App\Livewire\ProjectShowPage;
 use App\Livewire\ProjectsPage;
@@ -16,11 +14,9 @@ Route::prefix('projects')->name('projects.')->group(function (): void {
     Route::get('/', ProjectsPage::class)->name('index');
     Route::get('/{project}', ProjectShowPage::class)->name('show');
     Route::get('/{project}/runs/{pipelineRun}', ProjectRunPage::class)->name('runs.show');
-    Route::get('/{project}/lessons/{lesson}', ProjectLessonPage::class)->name('lessons.show');
 });
 
 Route::prefix('pipelines')->name('pipelines.')->group(function (): void {
     Route::get('/', PipelinesPage::class)->name('index');
     Route::get('/{pipeline}', PipelineShowPage::class)->name('show');
-    Route::get('/{pipeline}/steps/{step}', PipelineStepPage::class)->name('steps.show');
 });
